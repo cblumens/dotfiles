e0a125f02613902b0472872165fbb5a6fd9b0fce
Christian Blumensath <christian@blumensath.com>
2025-07-05 07:26:39 +0200

    Battle-tested CLI productivity stack

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -17,3 +17,8 @@ c.fonts.default_size = '12pt'
 
 c.content.local_content_can_access_file_urls = True
 c.content.local_content_can_access_remote_urls = True
+
+c.fileselect.handler = 'external'
+c.fileselect.single_file.command = ['zenity', '--file-selection']
+c.fileselect.multiple_files.command = ['zenity', '--file-selection', '--multiple']
+c.fileselect.folder.command = ['zenity', '--file-selection', '--directory']
