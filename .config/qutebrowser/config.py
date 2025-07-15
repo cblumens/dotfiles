37624ad8a4a9ed1372677f65140522fb234eca32
Christian Blumensath <christian@blumensath.com>
2025-07-15 22:13:35 +0200

    mutt: khard autocomplete instead aliases

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -13,7 +13,16 @@ c.colors.webpage.darkmode.policy.page = "smart"
 catppuccin.setup(c, 'mocha')      # Dunkel
 
 # Settings
-c.fonts.default_size = '12pt'
+c.fonts.default_size = '11pt'
 
 c.content.local_content_can_access_file_urls = True
 c.content.local_content_can_access_remote_urls = True
+
+
+c.url.searchengines = {
+    'DEFAULT': 'https://www.perplexity.ai/search?q={}'
+}
+
+
+c.url.start_pages = ['https://www.perplexity.ai/']
+c.url.default_page = 'https://www.perplexity.ai/'
